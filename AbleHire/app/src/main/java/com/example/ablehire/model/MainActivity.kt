5f6775edf462a72f85593c511e91cbdc45cbb78f
Prem Kumar <146103803+prem2493@users.noreply.github.com>
2025-02-19 18:21:10 +0530

    Add files via upload

---
+++ AbleHire/app/src/main/java/com/example/ablehire/model/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ablehire.model
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ablehire.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
