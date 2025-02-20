8a0b11ff60838136346f4f7a81b2bea9d177187f
Prem Kumar <146103803+prem2493@users.noreply.github.com>
2025-02-20 11:46:11 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import NavBar from './Components/NavBar';
+import Jobs from './Components/Job'
+import Community from './Components/community';
+import Jobpage from './Components/jobpage';
+import Courses from './Components/courses';
+import Jobpost from './Components/jobpost';
+import Settings from './Components/settings';
+import Profile from './Components/profile';
+import Resume from './Components/resume';
+import Chatbot from './Components/chatbot';
 import {AnimatePresence} from 'framer-motion';
 import Landing from './Components/landing';
 import Login from './Components/Login';
@@ -28,10 +37,75 @@ function App() {
       </>
     },
     {
+      path:"/community",
+      element:
+      <>
+      <NavBar/>
+      <Community/>
+      </>
+    },
+    {
+      path:"/courses",
+      element:
+      <>
+      <NavBar/>
+      <Courses/>
+      {/* <Profile/> */}
+      </>
+    },
+    {
+      path:"/jobpost",
+      element:
+      <>
+      <NavBar/>
+      <Jobpost/>
+      {/* <Profile/> */}
+      </>
+    },
+    {
+      path:"/profile",
+      element:
+      <>
+      <NavBar/>
+      <Profile/>
+      {/* <Profile/> */}
+      </>
+    },
+    {
+      path:"/resume",
+      element:
+      <>
+      <NavBar/>
+      <Resume/>
+      {/* <Chatbot/> */}
+      {/* <Profile/> */}
+      </>
+    },
+    {
+      path:"/settings",
+      element:
+      <>
+      <NavBar/>
+      <Settings/>
+      {/* <Profile/> */}
+      </>
+    },
+    {
+      path:"/chatbot",
+      element:
+      <>
+      <NavBar/>
+      <Chatbot/>
+      {/* <Profile/> */}
+      </>
+    },
+    {
       path:"/main",
       element:
       <>
       <NavBar/>
+      <Jobpage/>
+      {/* <Profile/> */}
       </>
     },
   ])
